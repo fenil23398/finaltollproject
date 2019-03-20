01fa8c40ab5fbdcbd3717b92f8c5461199344431
fenil23398 <fenilshah23398@gmail.com>
2019-03-20 16:33:08 +0530

    changes for payment

--- routes/payment_router.js
+++ routes/payment_router.js
@@ -3,12 +3,23 @@ var router = express.Router();
 var payment = require('../models/payment_model');
 
 router.get('/:id?',function(req,res){
+    if(req.params.id){
     payment.getPaymentDetailById(req.params.id,function(err,rows){
         if(err)
             res.json(err);
         else
             res.json(rows);
-    });
+        })
+    }
+    else{
+        payment.getAllPayments(function(err,rows){
+            if(err){
+                res.json(err);
+            }
+            else
+                res.json(rows);
+        })
+    }
 });
 
 router.post('/',function(req,res){
@@ -17,6 +28,24 @@ router.post('/',function(req,res){
             res.json(err);
         else    
             res.json(req.body);
+    });
+});
+
+router.put('/',function(req,res){
+    payment.updatePaymentById(req.body,function(err,rows){
+        if(err)
+            res.json(err);
+        else
+            res.json(rows);
+    })
+});
+
+router.delete('/:id?',function(req,res){
+    payment.deletePaymentId(req.params.id,function(err,count){
+        if(err)
+            res.json(err);
+        else
+            res.json(count);
     });
 });
 
