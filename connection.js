fadf0db7ec3471f066ef89fbd1a2ece3494f4950
fenil23398 <fenilshah23398@gmail.com>
2019-04-05 11:23:53 +0530

    new db

--- connection.js
+++ connection.js
@@ -1,8 +1,14 @@
 var mysql = require('mysql');
+// var cnn = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'toll_collection'    
+// });
 var cnn = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'toll_collection'    
+    host:'db4free.net',
+    user:'se_team_2',
+    password:'nopassword1234',
+    database:'toll_collection1'  
 });
 module.exports = cnn;
