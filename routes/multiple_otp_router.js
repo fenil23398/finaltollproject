d6f1f94b61cf6dc2ad4c354b45a0cbf0a5af9355
fenil23398 <fenilshah23398@gmail.com>
2019-04-11 22:06:56 +0530

    changes

--- routes/multiple_otp_router.js
+++ routes/multiple_otp_router.js
@@ -3,26 +3,32 @@ var router = express.Router();
 var motp = require('../models/multiple_otp');
 
 router.get('/:id?',function(req,res){
+    // var i=0;
+    // while(i<req.params.id){
+    var val = Math.floor(1000 + Math.random() * 900000);
     if(req.params.id){
-                motp.verifyotp(req.params.id,function(err,rows){
+                motp.verifyotp(val,function(err,rows){
                     if(err){
                         res.json(err);
                     }
                     else{
-                        if(res=="[]")
-                        {
-                            return res.json({
-                              msg:'no data'
-                            });
-                        }
-                        else{
-                            return res.json({
-                                msg: 'data'
-                            });
-                        }
+                        // if(res=="[]")
+                        // {
+                        //     return res.json({
+                        //       msg:'no data'
+                        //     });
+                        // }
+                        // else{
+                        //     return res.json({
+                        //         msg: 'data'
+                        //     });
+                        // }
+                        console.log(res);
+                        res.json(res);
                     }
                 })
           }
+        // }
     });
     
 
