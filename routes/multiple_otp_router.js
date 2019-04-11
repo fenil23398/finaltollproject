6d62862e6b9331b7bf360785988e052ebe887346
fenil23398 <fenilshah23398@gmail.com>
2019-04-11 22:12:02 +0530

    changes

--- routes/multiple_otp_router.js
+++ routes/multiple_otp_router.js
@@ -24,6 +24,7 @@ router.get('/:id?',function(req,res){
                         //     });
                         // }
                         console.log(res);
+                        
                         res.json(res);
                     }
                 })
