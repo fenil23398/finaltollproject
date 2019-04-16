68a068e672df7a79cc23150f1c005fd2d8290705
fenil23398 <fenilshah23398@gmail.com>
2019-04-16 14:15:07 +0530

    changes for change password

--- models/transaction_model.js
+++ models/transaction_model.js
@@ -2,7 +2,7 @@ var db=require('../connection');
 
 var trans={
    getTransactionByUserId:function(id,callback){
-       return db.query("select u.user_name,t.amount,t.isreturn,t.transaction_date,t.transaction_id,t.otp,t.transaction_time,t.status,pt.method_name,v.vehicle_no,vt.vehicle_type_name,tp.toll_plaza_id,tp.toll_name,tp.highway_name,tp.emergency_number,tp.city from user u,transaction t,vehicle v,vehicle_type vt,toll_plaza tp,payment_detail p,payment_method pt where t.user_id=u.user_id and t.toll_plaza_id=tp.toll_plaza_id and p.payment_id=t.payment_id and p.method_id=pt.method_id and v.vehicle_no=t.vehicle_no and v.vehicle_type_id=vt.vehicle_type_id and t.user_id=?",[id],callback);
+       return db.query("select u.user_name,t.amount,t.isreturn,t.transaction_date,t.transaction_id,t.otp,t.transaction_time,t.status,pt.method_name,v.vehicle_no,vt.vehicle_type_name,tp.toll_plaza_id,tp.toll_name,tp.highway_name,tp.emergency_number,tp.city from user u,transaction t,vehicle v,vehicle_type vt,toll_plaza tp,payment_detail p,payment_method pt where t.user_id=u.user_id and t.toll_plaza_id=tp.toll_plaza_id and p.payment_id=t.payment_id and p.method_id=pt.method_id and v.vehicle_no=t.vehicle_no and v.vehicle_type_id=vt.vehicle_type_id and t.user_id=? order by t.transaction_date",[id],callback);
    },
    addTransaction:function(tt,callabck){
        console.log("Transcation insert");
